797a1b3693bb3307b0e9278e1fc1b819466ebfa6
Zachary <zhowell01@rio.edu>
2025-09-21 22:31:36 -0400

    Debugged final output issues

--- node.py
+++ node.py
@@ -2,9 +2,7 @@ class Node:
 
 
     def __init__(self, value):
-        # Store the value given when the Node is created
         self.value = value
-        # Initialize next pointer as None until linked to another Node
         self.next = None
 
     def __repr__(self):
